fe74b10653f86239e7af8af15207e51c23cf021a
vennylee <hyungsup42134@gmail.com>
2022-02-10 05:27:47 +0900

    배너, 모달창 생성

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <!--  HEADER-->
+  <!-- Modal -->
+  <div class="madal" v-if="modalOpen">
+    <div class="black-bg">
+      <div class="white-bg">
+        <img src="" alt="" />
+        <h3>제목</h3>
+        <h4>텍스트</h4>
+      </div>
+    </div>
+  </div>
 
+  <!--  HEADER-->
   <div class="header">
     <a href="" v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
+  </div>
+
+  <!-- BANNER -->
+  <div class="banner">
+    <h3>지금 결제하면 30% 할인!</h3>
   </div>
 
   <!-- Card -->
   <div class="card">
     <div class="item" v-for="(product, i) in products" :key="i">
       <img :src="product.image" alt="" />
-      <h3>{{ product.title }}</h3>
+      <h3 @click="modalOpen = true">
+        {{ product.title }}
+      </h3>
       <h4>{{ product.price.toLocaleString() }}원</h4>
     </div>
   </div>
@@ -24,6 +41,7 @@ export default {
     return {
       menus: ["Home", "Product", "Info", "Contact"],
       products: products,
+      modalOpen: false,
     };
   },
 };
@@ -62,11 +80,32 @@ div {
   font-weight: 600;
 }
 
+.banner {
+  background-color: lightpink;
+  padding: 10px;
+  margin-top: 30px;
+}
+
 .item {
   margin-top: 30px;
 }
 
 .item img {
   width: 100%;
+}
+
+.black-bg {
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  padding: 100px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
