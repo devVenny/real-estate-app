10ae67f15e511e30541b4f3d5ee7ec85ea8cd5fc
vennylee <hyungsup42134@gmail.com>
2022-02-10 04:41:01 +0900

    헤더 생성

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <h2>Hello world!</h2>
+  <!--  HEADER-->
+
+  <div class="header">
+    <a href="" v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return { menus: ["Home", "Product", "Info", "Contact"] };
+  },
 };
 </script>
 
@@ -15,6 +22,25 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+}
+
+.header {
+  background-color: darkslateblue;
+  width: 100%;
+  padding: 20px;
+}
+
+.header a {
+  display: inline-block;
+  color: white;
+  text-decoration: none;
+  padding: 10px 5px;
+  margin-right: 20px;
+  border-radius: 2px;
+  font-weight: 600;
 }
 </style>
