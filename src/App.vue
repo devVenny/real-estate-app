7812e9ebc2d9796d3cb90ddfb32f4fe54d0afc83
vennylee <hyungsup42134@gmail.com>
2022-02-10 05:08:57 +0900

    카드 생성

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,46 @@
   <div class="header">
     <a href="" v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
   </div>
+
+  <!-- Card -->
+  <div class="card">
+    <div class="item" v-for="(product, i) in products" :key="i">
+      <img :src="product.image" alt="" />
+      <h3>{{ product.title }}</h3>
+      <h4>{{ product.price.toLocaleString() }}원</h4>
+    </div>
+  </div>
 </template>
 
 <script>
+import products from "./assets/products";
+
 export default {
   name: "App",
   data() {
-    return { menus: ["Home", "Product", "Info", "Contact"] };
+    return {
+      menus: ["Home", "Product", "Info", "Contact"],
+      products: products,
+    };
   },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-body {
-  margin: 0;
 }
 
 .header {
@@ -42,5 +60,13 @@ body {
   margin-right: 20px;
   border-radius: 2px;
   font-weight: 600;
+}
+
+.item {
+  margin-top: 30px;
+}
+
+.item img {
+  width: 100%;
 }
 </style>
