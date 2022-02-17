279c2da87189148abe85a593bcc43254e5641986
vennylee <hyungsup42134@gmail.com>
2022-02-17 13:09:01 +0900

    deploy.sh 파일 생성

--- vue.config.js
+++ vue.config.js
@@ -1,36 +1,3 @@
-const CopyPlugin = require("copy-webpack-plugin");
-const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-
 module.exports = {
-  entry: "./src/index.js",
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "./src/index.html",
-          to: `${__dirname}/dist/index.html`,
-        },
-        {
-          from: "./src/vader.css",
-          to: `${__dirname}/dist/vader.css`,
-        },
-      ],
-    }),
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new HtmlMinimizerPlugin(),
-      new CssMinimizerPlugin(),
-      new TerserPlugin(),
-    ],
-  },
-  output: {
-    path: `${__dirname}/dist`,
-    filename: "bundle.js",
-  },
-  mode: "production",
-  module: {},
+  publicPath: process.env.NODE_ENV === "production" ? "/real-estate-app/" : "/",
 };
